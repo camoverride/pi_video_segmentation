1f8a3085091deb3a96b257db95e3a2c907616126
cam <cam@cams-MacBook-Pro.local>
2024-08-15 21:51:54 -0700

    reorder

--- seg.py
+++ seg.py
@@ -1,3 +1,17 @@
+# Lint as: python3
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 r"""An example of semantic segmentation.
 
 The following command runs this script and saves a new image showing the
@@ -16,12 +30,12 @@ python3 examples/semantic_segmentation.py \
 
 import argparse
 
-import numpy as np
 from PIL import Image
 
 from pycoral.adapters import common
 from pycoral.adapters import segment
 from pycoral.utils.edgetpu import make_interpreter
+import numpy as np
 
 
 def create_pascal_label_colormap():
